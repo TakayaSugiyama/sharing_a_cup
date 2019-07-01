5cbfed861ad16398b1040637da17c665117abafa
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-01 12:21:45 +0000

    set users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/new'
-  get 'users/update'
-  get 'users/create'
+  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
