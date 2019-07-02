6e4756877f64260d347de70ce503391046ef2a8c
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-02 06:22:33 +0000

    add flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,22 +15,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save 
+      flash[:success] = "登録に成功しました。"
       redirect_to @user 
     else 
+      flash.now[:danger] = "登録に失敗しました。"
       render "users/new"
     end
   end
   
   def login_form
-    
+    @user = User.new
   end
   
-  def login 
-  end
+  
   
   private 
     def user_params 
-      params.require(:user).permit(:name,:email,:password)
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
-  
+    
 end
