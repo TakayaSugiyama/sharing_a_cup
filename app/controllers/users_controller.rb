e8e4e7a669415892f5035496469b491477e88ec2
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-02 02:14:13 +0000

    add signup form and installed bycrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  def index
-  end
-
   def show
   end
 
@@ -9,11 +6,31 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def update
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to @user 
+    else 
+      render "users/new"
+    end
   end
+  
+  def login_form
+    
+  end
+  
+  def login 
+  end
+  
+  private 
+    def user_params 
+      params.require(:user).permit(:name,:email,:password)
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "home#top"
-  get 'home/top'
-  resources :users
+  get 'signup',to: "users#new"
+  get 'login', to: "users#login_form"
+  post 'login',to:"users#login"
+  resources :users,only: [:show,:edit,:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
