3cfddad6084c8d08cfba9cbd2ffa35bf9f27c461
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-05 22:32:07 +0000

    add basic validation for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(email: params[:email])
-    password = params[:password]
+    @user = User.find_by(email: params[:user][:email])
+    password = params[:user][:password]
     if @user && @user.authenticate(password)
       session[:user_id] = @user.id
       flash[:success] = "ログインしました。"
       redirect_to user_path(@user)
     else
-      flash.now[:danger] = "ログインに失敗しました。"
+      flash.now[:danger] = "ログインできませんでした。"
       render "sessions/new"
     end
   end
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
    mount_uploader :image_name, ImageUploader
+   validates :name , presence: true , length: {in: 5..12} #5文字以上12文字以内
+   validates :email , presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                     uniqueness: true
    has_secure_password
    has_many :posts
 end
