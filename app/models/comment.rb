30d8318da77c075d3158541e1e78008d457da311
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-19 23:45:44 +0000

    コメントのバリデーションを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   has_many :comment_favorites, dependent: :destroy
   # バリデーション
+  validates :title, presence: true
+  validates :comment,presence: true,length: {maximum: 200}
 end
