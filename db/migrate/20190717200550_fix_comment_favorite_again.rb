4a33f32d0814794f17ec1ed7782ae4b61d2d0a2f
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-20 00:39:53 +0000

    fix migration

--- db/migrate/20190717200550_fix_comment_favorite_again.rb
+++
@@ -1,5 +0,0 @@
-class FixCommentFavoriteAgain < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :comment_favorites, :user,index: true
-  end
-end
--- db/migrate/20190719132756_add_post_column_to_commentfavorite.rb
+++
@@ -1,5 +0,0 @@
-class AddPostColumnToCommentfavorite < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :comment_favorites, :post, index: true
-  end
-end
