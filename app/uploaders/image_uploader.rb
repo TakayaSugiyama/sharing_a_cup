2b1dfc96b4f6e95a1e7d049b7d7c184f2081cded
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-24 05:09:57 +0000

    prepare for AWS S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,15 +5,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
     if Rails.env.production?
       storage :fog
-      config.fog_provider = 'fog/aws'
-      config.fog_directory  = 's3-sharing-a-sup'
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        region: ENV['AWS_REGION'],
-        path_style: true
-      }
     else
       storage :file
     end
