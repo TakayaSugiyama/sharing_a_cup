285ea835c7d1dc7592de65cff40617f7fbc74362
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-24 05:22:11 +0000

    methoを変えてみる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params) 
+    if @user.update_attributes(user_params) 
       flash[:success] = "ユーザー情報を更新しました"
       redirect_to user_path(@user)
     else 
