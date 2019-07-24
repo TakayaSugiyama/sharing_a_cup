eff6975c8bf68d4b81a101b5dbe665cc96e8c811
takaya.sugiyama <s.takaya1027@gmail.com>
2019-07-24 05:26:24 +0000

    少し書き方を変えた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params) 
+    @user.update_attributes(user_params) 
+    if @user.save
       flash[:success] = "ユーザー情報を更新しました"
       redirect_to user_path(@user)
     else 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  #include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
     if Rails.env.production?
@@ -24,12 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  process :resize_to_limit => [640, 480]
-  # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [300, 300]
-  end
-
+ 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_whitelist
